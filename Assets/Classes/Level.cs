2ed5fc010d2e42f9549ea8fb20bd4bf46c0b77db
JohannesGGE <johannesgge@gmail.com>
2021-12-15 18:56:35 +0100

    GameManager and LevelManager improvement

--- Assets/Classes/Level.cs
+++ Assets/Classes/Level.cs
@@ -1,11 +1,18 @@
 namespace Classes {
     public class Level {
         private Wave[] _waves;
+        private int _stars;
 
         public Level(Wave[] waves) {
             _waves = waves;
+            _stars = 0;
         }
 
         public Wave[] Waves => _waves;
+
+        public int Stars {
+            get => _stars;
+            set => _stars = value;
+        }
     }
 }
