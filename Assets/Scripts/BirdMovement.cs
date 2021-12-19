3358a58537fad3281f6bed03fab9993672f62329
Franziskus <79016562+franziskusz@users.noreply.github.com>
2021-12-19 16:39:09 +0100

    Scene for Testing Tower scripts
    - tracking and shooting Tower
    - shoots in direction of Enemy
    - Sting follows Enemy
    - Particle Effect on Impact

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      rb.velocity = Vector3.right*3f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      if(collision.gameObject.tag=="Tower")
+      {
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+      }
+    }
+}
