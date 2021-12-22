83cd3b4bf7f9c6b9a36dd0c5b0fff4767ba8398d
FranziskusZillmann <franziskus.zillmann@gmail.com>
2021-12-22 15:14:16 +0100

    improved pause functionality and stings gets effects from tower
    - Tower pauses game on 'space' key
    - Sting continues movement when Pause ends
    - TestBird also stops and can be destroyed by pressing 'd' during Pause
    - Damage and other Effect is given by the Tower to the Sting in the moment of shooting

--- Assets/Scripts/TowerScripts/Tower.cs
+++ Assets/Scripts/TowerScripts/Tower.cs
@@ -16,6 +16,10 @@ namespace Backbone
 
         private Transform target;
 
+        [Header("Hit Effects")]
+        public int towerDamage = 30;
+        public string towerEffect = "ice";
+
         [Header("Attributes")]
         public float range = 5f;
         public float fireRate = 1f;
@@ -40,27 +44,32 @@ namespace Backbone
         ///save all enemies in Range in an Array, <c>nearestEnemy</c> is the target
         void UpdateTarget()
         {
-          GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
-          float shortestDistance = Mathf.Infinity;
-          GameObject nearestEnemy = null;
+          ///while game is paused Enemy search is inactive. This should be unnecessary for game logic
+          ///since the targets do not move during pause anyways, but it might save some processing power.
+          if(!_gameManager.Paused)
+          {
+              GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+              float shortestDistance = Mathf.Infinity;
+              GameObject nearestEnemy = null;
 
-          //TODO Enemy selection based on distance of the enemy to the goal rather than the distance to the tower.
-          foreach (GameObject enemy in enemies)
-          {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if (distanceToEnemy < shortestDistance)
-            {
-              shortestDistance = distanceToEnemy;
-              nearestEnemy = enemy;
-            }
-          }
-          if (nearestEnemy != null && shortestDistance <= range)
-          {
-            target = nearestEnemy.transform;
-          }
-          else
-          {
-            target=null;
+              //TODO Enemy selection based on distance of the enemy to the goal rather than the distance to the tower.
+              foreach (GameObject enemy in enemies)
+              {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < shortestDistance)
+                {
+                  shortestDistance = distanceToEnemy;
+                  nearestEnemy = enemy;
+                }
+              }
+              if (nearestEnemy != null && shortestDistance <= range)
+              {
+                target = nearestEnemy.transform;
+              }
+              else
+              {
+                target=null;
+              }
           }
         }
 
@@ -91,28 +100,30 @@ namespace Backbone
           ///silence this method, while there is no enemy in Range
           if (target == null)
             return;
-
-          ///Rotate the firepoint to the Position of the target
-          ///3D Vector with the direction on Tower -> Target
-          Vector3 dir = target.position - transform.position;
-          ///Quaternion: Variable that manages rotation around all three axis.
-          ///Mathematically it is a number system that extends real numbers with i, j and k instead just with the i(maginary part) of complex numbers.
-          //Thus it forms the quotient of two vectors in three dimensional space.
-          //The <c>Lerp</c> is only used to make the movement of the rotation smoother.
-          Vector3 rotatedVectorDir = Quaternion.Euler(0,0,180)*dir;
-          Quaternion lookRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: rotatedVectorDir);
-          Quaternion rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed);
-          partToRotate.rotation = rotation;
-
-          //shoot in given intervall, if the game is not paused
-          if (fireCountdown <= 0f && !_gameManager.Paused)
+          ///while game is paused target tracking, <c>Shoot()</c> and <c>fireCountdown</c> are inactive
+          if(!_gameManager.Paused)
           {
-            Shoot();
-            fireCountdown = 1f / fireRate;
+              ///Rotate the firepoint to the Position of the target
+              ///3D Vector with the direction on Tower -> Target
+              Vector3 dir = target.position - transform.position;
+              ///Quaternion: Variable that manages rotation around all three axis.
+              ///Mathematically it is a number system that extends real numbers with i, j and k instead just with the i(maginary part) of complex numbers.
+              //Thus it forms the quotient of two vectors in three dimensional space.
+              //The <c>Lerp</c> is only used to make the movement of the rotation smoother.
+              Vector3 rotatedVectorDir = Quaternion.Euler(0,0,180)*dir;
+              Quaternion lookRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: rotatedVectorDir);
+              Quaternion rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed);
+              partToRotate.rotation = rotation;
+
+              //shoot in given intervall
+              if (fireCountdown <= 0f)
+              {
+                Shoot();
+                fireCountdown = 1f / fireRate;
+              }
+
+              fireCountdown -= Time.deltaTime;
           }
-
-          fireCountdown -= Time.deltaTime;
-
         }
 
         void Shoot()
@@ -122,6 +133,8 @@ namespace Backbone
           Sting sting = stingGO.GetComponent<Sting>();
           if (sting != null)
           {
+            sting.damage=towerDamage;
+            sting.effect=towerEffect;
             sting.Seek(target);
           }
         }
