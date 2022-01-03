9434468bc867b35ef6199fc8bec0324b45496cf7
MobyBig <tobi.marr@freenet.de>
2022-01-03 21:43:08 +0100

    level unlock eingefügt

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
@@ -25,4 +26,54 @@ public class LevelSelect : MonoBehaviour
     { SceneManager.LoadScene("LevelNine"); }
     public void StartLevelTen()
     { SceneManager.LoadScene("LevelTen"); }
+
+
+    public Button[] levelButtons;
+    public GameObject child;
+    public Sprite zeroStar;
+    public Sprite oneStar;
+    public Sprite twoStar;
+    public Sprite threeStar;
+    public int starsReached;
+
+
+
+    private void Start()
+    {
+
+        int levelSolved = PlayerPrefs.GetInt("levelSolved", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            child = levelButtons[i].transform.GetChild(1).gameObject; //muss erst in child gespeichert werden, damit änderungen möglich sind
+
+            if (i > levelSolved)
+            { levelButtons[i].interactable = false;
+                
+                child.GetComponent<Image>().color = Color.grey;
+
+               
+
+                Debug.Log("Kind: " + child.name);
+                Debug.Log("gefundene Sterne: " + levelButtons[i].transform.GetChild(1).name + GameObject.Find("Stars").GetComponent<Image>().color);   
+            }
+            else
+            {
+                if (starsReached == 0)
+                { child.gameObject.GetComponent<Image>().sprite = zeroStar; }
+                else if (starsReached == 1)
+                { child.gameObject.GetComponent<Image>().sprite = oneStar; }
+                else if (starsReached == 2)
+                { child.gameObject.GetComponent<Image>().sprite = twoStar; }
+                else if (starsReached == 3)
+                { child.gameObject.GetComponent<Image>().sprite = threeStar; }
+            }
+
+
+
+        }
+    }
+
+
 }
+
