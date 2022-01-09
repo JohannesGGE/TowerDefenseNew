214cd76789973ab1b094867915dc24c2017dceea
JohannesGGE <johannesgge@gmail.com>
2022-01-09 12:25:13 +0100

    Spawnrate in der Wave implementiert. - Birds in BirdLevel umbenannt um Bedeutung klarer zu machen - WaveSpawner: Zugriff auf enemy Attribut enzogen

--- Assets/Scripts/Classes/Wave.cs
+++ Assets/Scripts/Classes/Wave.cs
@@ -7,26 +7,34 @@ namespace Classes {
     /// </summary>
     public class Wave {
 
-        public GameObject enemy;
-        public int count;
-        public float rate;
-
         /// <summary>
         /// Variable <c>_birds</c> enthaelt die Voegel der Welle
         /// </summary>
-        private Birds[] _birds;
+        private BirdLevel[] _birds;
+        
+        /// <summary>
+        /// Variable <c>_spawnRate</c> enthaelt zu spawnenden Voegeln pro Sekunde
+        /// </summary>
+        private float _spawnRate;
         
         /// <summary>
         /// Konstruktor, setzt die Uebergebenen Voegel
         /// </summary>
         /// <param name="birds">zu setzende Voegel</param>
-        public Wave(Birds[] birds) {
+        /// <param name="spawnRate">wie viele pro Sekunde</param>
+        public Wave(BirdLevel[] birds, float spawnRate) {
             _birds = birds;
+            _spawnRate = spawnRate;
         }
-        
+
         /// <summary>
-        /// enthaelt die Voegel der Welle
+        /// enthaelt die Zeit zwischen zwei zu spawnenden Voegeln
         /// </summary>
-        public Birds[] Birds => _birds;
+        public float SpawnRate => _spawnRate;
+
+        /// <summary>
+        /// enthaelt zu spawnenden Voegeln pro Sekunde
+        /// </summary>
+        public BirdLevel[] Birds => _birds;
     }
 }
