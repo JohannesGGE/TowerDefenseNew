3814c000c5760cf69d2253f94ac4d859b50b4d3c
MobyBig <tobi.marr@freenet.de>
2022-01-21 16:31:55 +0100

    GameValues angepasst, CheatTaste M für mehr Money eingefügt, Range Ring beim drag sichtbar

--- Assets/Scripts/GUI Scripts/Drop.cs
+++ Assets/Scripts/GUI Scripts/Drop.cs
@@ -1,3 +1,4 @@
+using Classes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,43 +13,69 @@ public class Drop : MonoBehaviour, IDropHandler
    
     private bool isTower = false;
 
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameManager.GetInstance();
+    }
+
     /// <summary>
     /// Methode die ausgefuehrt wird, wenn ein Objekt losgelassen wird, nach Drag
     /// </summary>
     /// <param name="eventData"> Objekt welches gedraggt wird </param>
     public void OnDrop(PointerEventData eventData) {
 
+        if (eventData != null)
+        { 
         //Tower werden im Slot platziert und können so angesprochen werden
         FolderToBuild = gameObject.transform.GetChild(0);
         
-       
-
         Debug.Log("OnDrop");
-        
-        if(CompareTag("TowerSlot") && !isTower) {
-            // TODO entscheiden, ob Genug Geld
-            // mit eventData.pointerDrag.name kann man das Object bekommen, wo es hergezogen wurde
 
-            Debug.Log("Ausgewählter Kaktus: " + eventData.pointerDrag.name);
+            if (CompareTag("TowerSlot") && !isTower)
+            {
+                // TODO entscheiden, ob Genug Geld
+                // mit eventData.pointerDrag.name kann man das Object bekommen, wo es hergezogen wurde
 
-            // Ueberpruefung welche Farbe der gedraggte Kaktus hat
-            switch (eventData.pointerDrag.name) {
-                case "Red":
-                    _tower = Resources.Load("TowerPrefabs/TowerRed") as GameObject;
-                    break;
-                case "Green":
-                    _tower = Resources.Load("TowerPrefabs/TowerGreen") as GameObject;
-                    break;
-                case "Yellow":
-                    _tower = Resources.Load("TowerPrefabs/TowerBlue") as GameObject;
-                    break;
+                Debug.Log("Ausgewählter Kaktus: " + eventData.pointerDrag.name);
+
+                // Ueberpruefung welche Farbe der gedraggte Kaktus hat
+                switch (eventData.pointerDrag.name)
+                {
+                    case "Red":
+                        if (_gameManager.Coins >= GameValues.PriceFireTower)
+                        {
+                            _tower = Resources.Load("TowerPrefabs/TowerRed") as GameObject;
+                            _gameManager.ReduceCoins(GameValues.PriceFireTower);
+                        }
+                        break;
+                    case "Green":
+                        if (_gameManager.Coins >= GameValues.PriceBasicTower)
+                        {
+                            _tower = Resources.Load("TowerPrefabs/TowerGreen") as GameObject;
+                            _gameManager.ReduceCoins(GameValues.PriceBasicTower);
+                        }
+                        //else
+                        //{ Debug.Log("zu wenig money"); }
+                        break;
+                    case "Blue":
+                        if (_gameManager.Coins >= GameValues.PriceIceTower)
+                        {
+                            _tower = Resources.Load("TowerPrefabs/TowerBlue") as GameObject;
+                            _gameManager.ReduceCoins(GameValues.PriceIceTower);
+                        }
+                        //else
+                        //{ Debug.Log("zu wenig money"); }
+                        break;
+                }
+
+                //Kaktus wird platziert
+                GameObject newT = Instantiate(_tower, GetComponent<RectTransform>().anchoredPosition, Quaternion.identity, FolderToBuild);
+                newT.transform.position = GetComponent<RectTransform>().transform.position;
+
+                isTower = true;
             }
-
-            //Kaktus wird platziert
-            GameObject newT = Instantiate(_tower, GetComponent<RectTransform>().anchoredPosition, Quaternion.identity, FolderToBuild);
-            newT.transform.position = GetComponent<RectTransform>().transform.position;
-            
-            isTower = true;
         }
     }
 }
